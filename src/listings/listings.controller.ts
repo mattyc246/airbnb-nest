82198f0ec68d4746c0a50971d76069daa3b4e56d
Matthew Cross <mattyc246@gmail.com>
2020-11-26 14:17:25 +0800

    user can create listing

--- src/listings/listings.controller.ts
+++ src/listings/listings.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, UseGuards, Req, UseInterceptors } from '@nestjs/common';
 import { ListingsService } from './listings.service';
 import { CreateListingDto } from './dto/create-listing.dto';
 import { UpdateListingDto } from './dto/update-listing.dto';
 import JwtAuthenticationGuard from 'src/authentication/guards/jwtAuthentication.guard';
 import { RequestWithUser } from 'src/authentication/interfaces/requestWithUser.interface';
+import { TransformInterceptor } from 'src/users/transform.interceptor';
 
 @Controller('listings')
+@UseInterceptors(TransformInterceptor)
 export class ListingsController {
   constructor(private readonly listingsService: ListingsService) {}
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Body, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, UseGuards, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from "./dto/create-user.dto"
 import { UsersService } from "./users.service"
 import { User } from "./entities/user.entity"
 import { RequestWithUser } from '../authentication/interfaces/requestWithUser.interface';
 import JwtAuthenticationGuard from '../authentication/guards/jwtAuthentication.guard';
+import { TransformInterceptor } from './transform.interceptor';
 
 @Controller('users')
+@UseInterceptors(TransformInterceptor)
 export class UsersController {
   constructor(private usersService: UsersService){}
 
